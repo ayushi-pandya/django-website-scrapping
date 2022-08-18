a80a7ecaa8e2954d10d2231c286e9f1361e4268c
ayushi-pandya <ayushi.inexture@gmail.com>
2022-08-18 19:14:54 +0530

    changes

--- demo/views.py
+++ demo/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
@@ -7,8 +6,6 @@ from django.views import View
 
 
 def search_data(search):
-    names = []
-    information = []
     HEADER = {
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
     PROXY = {"http": "http//80.48.119.28:8080"}
@@ -19,9 +16,16 @@ def search_data(search):
     soup = BeautifulSoup(page_html, 'html.parser')
 
     movies = soup.find('table', class_='findList').find_all('td', class_='result_text')
-    for movie in movies[:5]:
+
+    name_list = []
+    des_list = []
+    director_list = []
+    writer_list = []
+    year_list = []
+
+    for movie in movies[:3]:
         name = movie.a.text
-        names.append(name)
+        name_list.append(name)
 
         data = movie.find('a', href=True)
         res = requests.get('https://www.imdb.com' + data['href'])
@@ -31,31 +35,34 @@ def search_data(search):
         details = soup.find('section', class_='ipc-page-section ipc-page-section--baseAlt ipc-page-section--tp-none '
                                               'ipc-page-section--bp-xs sc-2a827f80-1 gvCXlM').find_all('div',
                                                                                                        class_='sc-2a827f80-10 fVYbpg')
-        # print(details)
+
         for detail in details:
             des = detail.find('div', class_='sc-16ede01-7 hrgVKw').text
+            des_list.append(des)
+
             director = detail.find('ul',
                                    class_='ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt').text
+            director_list.append(director)
+
             writer = detail.find_next('ul',
                                       class_='ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt').find_next(
                 'ul',
                 class_='ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt').text
-            year = detail.find('li', class_='ipc-inline-list__item').text
-            print('director:', director)
-            print('writer:', writer)
-            print('star:', year)
-    # 'ipc-inline-list__item'
-    # 'ipc-inline-list__item'
-    # print('name:',name['href'])
-    # print('name:',name.split()[1])
+            writer_list.append(writer)
 
-    # info = movie.text
-    # # print('name:', info[1])
-    #
-    # information.append(info)
+        info = soup.find('section', class_='ipc-page-section ipc-page-section--baseAlt ipc-page-section--tp-none '
+                                           'ipc-page-section--bp-xs sc-2a827f80-1 gvCXlM').find_all('div',
+                                                                                                    class_='sc-80d4314-2 iJtmbR')
 
-    details_dict = {'MovieName': names,
-                    'MovieInfo': information}
+        for i in info:
+            year = i.text
+            year_list.append(year)
+
+    details_dict = {'Name': name_list,
+                    'Description': des_list,
+                    'Director': director_list,
+                    'Writer': writer_list,
+                    'Year': year_list}
 
     return details_dict
 
@@ -70,9 +77,31 @@ class DemoFunction(View):
         if search != '':
             names = search_data(search)
             a = {}
-            for i in range(len(names['MovieName'])):
-                a[names['MovieName'][i]] = names['MovieInfo'][i]
-            return render(request, 'demo/demo.html', {'data': a})
+            for i in range(len(names['Name'])):
+                a[names['Name'][i]] = names['Description'][i], names['Director'][i], names['Writer'][i], names['Year'][
+                    i]
+            return render(request, 'demo/home.html', {'data': a})
 
         else:
             return render(request, 'demo/demo.html')
+
+
+# user_input = request.GET.get('inputValue')
+#     data = {'response': f'You typed: {user_input}'}
+#     return JsonResponse(data)
+
+# class Search(View):
+# def all_search(request):
+#     search = request.POST['search']
+#     print('search:', search)
+#     search = search.strip()
+#     if search != '':
+#         names = search_data(search)
+#         a = {}
+#         for i in range(len(names['Name'])):
+#             a[names['Name'][i]] = names['Description'][i], names['Director'][i], names['Writer'][i], names['Year'][
+#                 i]
+#         return render(request, 'demo/home.html', {'data': a})
+#
+#     else:
+#         return render(request, 'demo/demo.html')
